b599154c28ae3aee9973567b83f53ec70fddbe30
Bruno Simplicio <bruno@notabene.id>
2023-06-12 22:38:02 -0400

    improving coverage

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,9 +9,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user"); // Generate a random address
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // Sets the balance of an address
     }
 
     function testMinimumDollarIsFive() public {
@@ -25,5 +30,17 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Next line should revert
+        fundMe.fund(); // This fails because the ETH sent is 0
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next TX will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
